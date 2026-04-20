b050fd2b45c185306722fc6879fdc1ee35730ad0
deveshprakash2929-ux <deveshprakash2929@gmail.com>
2026-04-20 20:19:52 +0530

    Implement parity check for odd numbers in range

---
+++ #include <iostream> using namespace std; int main() { long long L, R; cin >> L >> R; int oddCount = (R + 1) / 2 - (L /Help Alice to find the Parity.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+long long L, R;
+cin >> L >> R;
+int oddCount = (R + 1) / 2 - (L / 2);
+if (oddCount % 2 == 0)
+     cout << "even";
+ else
+    cout << "odd";
+ return 0;
+}
